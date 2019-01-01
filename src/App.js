18b61e4070df8a5d857b236826573849a166437a
Manuel Lao <manolin@iMac-de-Manuel.local>
2019-01-01 21:21:21 +0100

    uso de flex-grid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
 import './App.css';
 
@@ -19,10 +24,25 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        Weather App (aplicación del clima)
-        <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation}></LocationList>
-      </div>
+        <Grid>
+          <Row>
+            <AppBar position='sticky'>
+              <Toolbar>
+                <Typography variant='title' color='inherit'>
+                  Weather App
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation}></LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <div className="details"></div>
+            </Col>
+          </Row>
+        </Grid>
     );
   }
 }
